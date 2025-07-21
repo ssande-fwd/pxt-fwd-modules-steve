228ea72e3956752bbdc643a12a628c3074741f3a
ssande-fwd <ssande@forwardedu.com>
2025-07-21 14:21:53 -0400

    lcd fix

---
+++ fwd-lcd/test.ts
@@ -0,0 +1,7 @@
+input.onButtonPressed(Button.A, function () {
+    fwdLights.characterDisplay1.showString("test123")
+})
+input.onButtonPressed(Button.B, function () {
+    fwdLights.characterDisplay1.showString("test123")
+})
+basic.forever(function () {})
